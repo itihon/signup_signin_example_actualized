b8e5adde25d45c4b8a6662472c250476e0ecc411
itihon <kalabin.alexandr@gmail.com>
2025-04-28 16:04:26 +0500

    feat(apply-effect.js): update according to quick_introduction branch

--- validation/ui/apply-effects.js
+++ validation/ui/apply-effects.js
@@ -1,34 +1,68 @@
+import { Validation } from 'isomorphic-validation';
 import { applyAccess, applyOutline, applyBox, renderFirstError } from 'isomorphic-validation/ui';
 
-const errMsgEID = 'ERROR_MSG';
+const delayedOutline = {
+  false: { delay: 2000, value: '2px solid lightpink' },
+  true: { delay: 500, value: '' },
+};
+
+const delayedAccess = {
+  true: { delay: 600 },
+};
+
+const changedOutline = {
+  false: { value: '2px solid lightpink' },
+  true: { delay: 500, value: '' },
+};
+
+const editIcon = {
+  false: { value: '🖊' },
+  true: { value: '🖊' },
+  position: 'LEVEL_RIGHT',
+};
+
+const validIcon = {
+  false: { delay: 1000, value: '' },
+  true: { delay: 500, value: '✔' },
+  position: 'LEVEL_RIGHT_BESIDE',
+  style: { color: 'green', left: '-8px' },
+};
 
 const errMsg = {
-  false: { value: renderFirstError('err'), delay: 2000 },
+  false: { delay: 2000, value: renderFirstError('err') },
   position: 'BELOW_CENTER',
   mode: 'MAX_SIDE',
-  style: {
-    color: 'firebrick',
-    fontSize: '12px',
-  }
+  style: { color: 'firebrick', fontSize: '12px', padding: '4px' },
 };
 
 const applyEffects = (validationProfile) => {
   const [form, groupingValidation] = validationProfile;
-  
-  groupingValidation.validations.forEach(
-    (validation, idx) => validation
-      .client
-      .validated(applyOutline(form[idx].parentNode))
-      .changed(applyOutline(form[idx].parentNode)) // needed for glued validations
-      .started(applyBox(form[idx].parentNode, errMsgEID))
-      .validated(applyBox(errMsg, form[idx].parentNode, errMsgEID))
-  );
 
-  groupingValidation
+  form.addEventListener(
+    'input',
+    Validation.group(
+      groupingValidation.validations.map(
+        (validation, idx) => {
+
+          const iconEID = form[idx].name + 'icon';
+          const errMsgEID = form[idx].name + 'error';
+        
+          return validation
+            .client
+            .validated(applyOutline(form[idx].parentNode, delayedOutline))
+            .changed(applyOutline(form[idx].parentNode, delayedOutline)) // needed for glued validations
+            .changed(applyOutline(form[idx].parentNode, changedOutline))
+            .started(applyBox(form[idx].parentNode, editIcon, iconEID))
+            .validated(applyBox(form[idx].parentNode, validIcon, iconEID))
+            .changed(applyBox(form[idx].parentNode, validIcon, iconEID)) // needed for glued validations
+            .validated(applyBox(form[idx].parentNode, errMsg, errMsgEID))
+            .changed(applyBox(form[idx].parentNode, errMsg, errMsgEID)); // nedded for glued validations
+        }
+      )
+    )
     .client
-    .changed(applyAccess(form.submitBtn));
-
-  form.addEventListener('input', groupingValidation);
+    .changed(applyAccess(form.submitBtn, delayedAccess))
+  );
 };
 
 export default applyEffects;
