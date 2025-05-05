77d728f15573fb2b795600dc002e51f0d1f15bde
itihon <kalabin.alexandr@gmail.com>
2025-05-05 17:49:30 +0500

    feat: add length constraints form e-mail field

--- validation/validations/email.js
+++ validation/validations/email.js
@@ -1,9 +1,11 @@
 import { Validation } from 'isomorphic-validation';
-import isMaxLength from '../predicates/is-max-length.js';
 import isEmail from '../predicates/is-email.js';
+import isMinLength from '../predicates/is-min-length.js';
+import isMaxLength from '../predicates/is-max-length.js';
 
 const emailV = Validation()
-  .constraint(isEmail, { next: false })
-  .constraint(isMaxLength(64), { next: false });
+  .constraint(isMinLength(8), { next: false })
+  .constraint(isMaxLength(48), { next: false })
+  .constraint(isEmail, { next: false });
 
 export default emailV;
